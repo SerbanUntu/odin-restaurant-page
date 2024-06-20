9e28ee6eaeebf1e493f78530c4f8367ba4689416
Serban Untu <133024956+SerbanUntu@users.noreply.github.com>
2024-06-20 15:57:33 +0300

    feat: homepage and about page

--- src/components/about.js
+++ src/components/about.js
@@ -3,6 +3,43 @@ import aboutData from './about.json';
 export default function loadAboutTab () {
   const content = document.createElement('div');
   content.id = 'content';
-  content.innerHTML = `<h1>About</h1>`;
+  content.innerHTML = `
+    <h1>About us</h1>
+    <h2>History</h2>
+    <p class="indent">${aboutData.history}</p>
+    <h2>Key members</h2>
+    <ul>
+      <li>
+        <p><span class="employee-title">${aboutData.employees[0].title}</span> <span class="employee-name">${aboutData.employees[0].name}</span></p>
+        <p>${aboutData.employees[0].bio}</p>
+      </li>
+      <li>
+        <p><span class="employee-title">${aboutData.employees[1].title}</span> <span class="employee-name">${aboutData.employees[1].name}</span></p>
+        <p>${aboutData.employees[1].bio}</p>
+      </li>
+      <li>
+        <p><span class="employee-title">${aboutData.employees[2].title}</span> <span class="employee-name">${aboutData.employees[2].name}</span></p>
+        <p>${aboutData.employees[2].bio}</p>
+      </li>
+    </ul>
+    <h2>Contact information</h2>
+    <ul>
+      <li>
+        <p>${aboutData.contact.phone}</p>
+      </li>
+      <li>
+        <p>${aboutData.contact.email}</p>
+      </li>
+      <li>
+        <p>${aboutData.contact.socialMedia.instagram} (instagram)</p>
+      </li>
+      <li>
+        <p>${aboutData.contact.socialMedia.twitter} (twitter)</p>
+      </li>
+      <li>
+        <p>${aboutData.contact.socialMedia.youtube} (youtube)</p>
+      </li>
+    </ul>
+  `;
   return content;
 }
--- src/components/home.js
+++ src/components/home.js
@@ -3,6 +3,35 @@ import homeData from './home.json';
 export default function loadHomeTab () {
   const content = document.createElement('div');
   content.id = 'content';
-  content.innerHTML = `<h1>Home</h1>`;
+  content.innerHTML = `
+    <h1>${homeData.name}</h1>
+    <p class="indent">${homeData.description}</p>
+    <h2>Hours</h2>
+    <ul>
+      <li>
+        <p><span class="week-day">${homeData.hours[0].day}: </span>${homeData.hours[0].start} - ${homeData.hours[0].end}</p>
+      </li>
+      <li>
+        <p><span class="week-day">${homeData.hours[1].day}: </span>${homeData.hours[1].start} - ${homeData.hours[1].end}</p>
+      </li>
+      <li>
+        <p><span class="week-day">${homeData.hours[2].day}: </span>${homeData.hours[2].start} - ${homeData.hours[2].end}</p>
+      </li>
+      <li>
+        <p><span class="week-day">${homeData.hours[3].day}: </span>${homeData.hours[3].start} - ${homeData.hours[3].end}</p>
+      </li>
+      <li>
+        <p><span class="week-day">${homeData.hours[4].day}: </span>${homeData.hours[4].start} - ${homeData.hours[4].end}</p>
+      </li>
+      <li>
+        <p><span class="week-day">${homeData.hours[5].day}: </span>${homeData.hours[5].start} - ${homeData.hours[5].end}</p>
+      </li>
+      <li>
+        <p><span class="week-day">${homeData.hours[6].day}: </span>${homeData.hours[6].start} - ${homeData.hours[6].end}</p>
+      </li>
+    </ul>
+    <h2>Location</h2>
+    <p class="text-center">${homeData.location}</p>
+  `;
   return content;
 }
